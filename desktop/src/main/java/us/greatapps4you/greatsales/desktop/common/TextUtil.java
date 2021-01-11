a83b66f254ad3f186e5ee3c74ae32652739230fd
Jose E. de Souza N <developer@greatapps4you.us>
2021-01-11 12:12:49 +0100

    [desktop] updated Copyright

--- desktop/src/main/java/us/greatapps4you/greatsales/desktop/common/TextUtil.java
+++ desktop/src/main/java/us/greatapps4you/greatsales/desktop/common/TextUtil.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021. GreatApps4you LLC
+ * This Software is licenced to you according to the GNU GENERAL PUBLIC LICENSE
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + SNMV SMQL IVB
+ */
+
 package us.greatapps4you.greatsales.desktop.common;
 
 import java.util.Arrays;
