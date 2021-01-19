715b105b47515d2ee5d2187bbb188623db612c8b
Jose E. de Souza N <developer@greatapps4you.us>
2021-01-19 08:07:05 +0100

    [desktop] with Springboot set to version 2.1.1.RELEASE, H2 set to scop runtime and with the new TestingConfig and TestingJpaConfig config classes, SpringBoot DataJpaTest framework is now working

---
+++ desktop/src/test/java/us/greatapps4you/greatsaltes/desktop/controllers/ProductTabControllerTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 GreatApps4you LLC
+ * This Software is licenced under the GNU GENERAL PUBLIC LICENSE v3
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + SNMV SMQL IVB
+ */
+
+package us.greatapps4you.greatsaltes.desktop.controllers;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import us.greatapps4you.greatsales.entities.inventory.Product;
+import us.greatapps4you.greatsaltes.desktop.repositories.ProductRepository;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+@ContextConfiguration(classes = {TestingConfig.class, TestingJpaConfig.class})
+public class ProductTabControllerTest {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void testPersistProduct() {
+        UUID uuid = UUID.randomUUID();
+        String sku = "TEST_PRODUCT_SKU";
+        String description = "BORRACHA_TEST";
+
+        Product given = Product.builder()
+                .uuid(uuid)
+                .sku(sku)
+                .description(description)
+                .build();
+
+        repository.save(given);
+
+        Product actual = repository.findById(uuid).get();
+        Assert.assertEquals(actual.getUuid(), uuid);
+        assertThat(actual.getUuid()).isEqualTo(uuid);
+
+        System.out.println(actual);
+    }
+}
---
+++ desktop/src/test/java/us/greatapps4you/greatsaltes/desktop/controllers/TestingConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 GreatApps4you LLC
+ * This Software is licenced under the GNU GENERAL PUBLIC LICENSE v3
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + SNMV SMQL IVB
+ */
+
+package us.greatapps4you.greatsaltes.desktop.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestingConfig {
+}
