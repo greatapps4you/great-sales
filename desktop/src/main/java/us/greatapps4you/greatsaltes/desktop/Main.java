5c9305bfd898cfa5c0a75e22c0b8100939d2adc1
Jose E. de Souza N <developer@greatapps4you.us>
2021-01-28 15:52:26 +0100

    [desktop] icon and Title to Main Stage

--- desktop/src/main/java/us/greatapps4you/greatsaltes/desktop/Main.java
+++ desktop/src/main/java/us/greatapps4you/greatsaltes/desktop/Main.java
@@ -12,6 +12,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -38,6 +39,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(rootNode));
+        stage.setTitle("GreatSales");
+        stage.getIcons().add(new Image(ClassLoader.getSystemResourceAsStream("images/icon.png")));
         stage.show();
     }
 
