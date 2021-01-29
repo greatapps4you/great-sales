44aecff91f7d713d88870797661ca216b3bf041d
Jose E. de Souza N <developer@greatapps4you.us>
2021-01-29 23:17:16 +0100

    [desktop] persistence is working

--- desktop/src/main/java/us/greatapps4you/greatsales/GreetingResource.java
+++ desktop/src/main/java/us/greatapps4you/greatsales/GreetingResource.java
@@ -1,16 +1,32 @@
 package us.greatapps4you.greatsales;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDateTime;
+import java.util.List;
 
-@Path("/hello-resteasy")
+@Path("/product/save")
 public class GreetingResource {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Product save() {
+        Product product = new Product();
+        product.setDescription(LocalDateTime.now().toString());
+        return repository.save(product);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Product> list() {
+        //return repository.findAll().iterator();
+        return null;
     }
 }
