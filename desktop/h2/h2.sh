4a25eb87f1e407cb4170eb085703b27077eb3c9d
Jose E. de Souza N <developer@greatapps4you.us>
2021-02-01 15:33:31 +0100

    [desktop] updated Copyright

--- desktop/h2/h2.sh
+++ desktop/h2/h2.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+#
+# Copyright (c) 2021 GreatApps4you LLC
+# This Software is licenced under the GNU GENERAL PUBLIC LICENSE v3
+# https://www.gnu.org/licenses/gpl-3.0.txt
+# https://greatapps4you.us
+# CSSML NDSMD VRS + SNMV SMQL IVB
+#
+
 dir=$(dirname "$0")
 java -cp "$dir/h2-1.4.199.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
