357bebb9eb2a9f6a9dc21f1c8edaaa758f7a640f
Jose E. de Souza N <developer@greatapps4you.us>
2021-02-01 19:05:46 +0100

    [desktop] allow create database when not exists

--- desktop/h2/h2.sh
+++ desktop/h2/h2.sh
@@ -8,4 +8,4 @@
 #
 
 dir=$(dirname "$0")
-java -cp "$dir/h2-1.4.199.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
+java -cp "$dir/h2-1.4.199.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Server -ifNotExists "$@"
