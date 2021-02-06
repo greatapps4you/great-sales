ca8946abae6ed670b5e5e13a5d03c1021095b568
Jose E. de Souza N <developer@greatapps4you.us>
2021-02-06 17:38:18 +0100

    [desktop] added customer backend persistence

---
+++ desktop/src/main/java/us/greatapps4you/greatsales/controllers/CustomerController.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2021 GreatApps4you LLC
+ * This Software is licenced under the GNU GENERAL PUBLIC LICENSE v3
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ * https://greatapps4you.us
+ * CSSML NDSMD VRS + SNMV SMQL IVB
+ */
+
+package us.greatapps4you.greatsales.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import us.greatapps4you.greatsales.entities.order.Customer;
+import us.greatapps4you.greatsales.repositories.CustomerRepository;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Controller
+@Path("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository repository;
+    @Value("${greatsales.customer.home}")
+    private String CUSTOMER_HOME;
+
+    @POST
+    @Path("save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Customer save(Customer customer) {
+        System.out.println("Customer Data Received: " + customer);
+
+        if (customer != null) {
+            if (customer.getUuid() == null) {
+                customer.setUuid(UUID.randomUUID());
+            }
+        }
+        return repository.save(customer);
+    }
+
+    @GET
+    @Path("find/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Customer find(@PathParam("id") UUID id) {
+        return repository.findById(id).orElse(Customer.builder().build());
+    }
+
+    @GET
+    @Path("remove/{id}")
+    @Produces(MediaType.TEXT_HTML)
+    public String delete(@PathParam("id") UUID id) {
+        try {
+            repository.deleteById(id);
+            //FIXME: When we have a serious view framework like React this concern goes away
+            return "<script>window.location.replace('" + CUSTOMER_HOME + "')</script>";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ERROR: " + e.getLocalizedMessage();
+        }
+    }
+
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Customer> list() {
+        List<Customer> allCustomers = new ArrayList<>();
+        repository.findAll().iterator().forEachRemaining(allCustomers::add);
+        return allCustomers;
+    }
+}
