9f5b638a05ee6b3bc3b8dbac7c7461a98107581c
Jose E. de Souza N <developer@greatapps4you.us>
2021-02-13 14:11:03 +0100

    [web] updated copyright

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2021 GreatApps4you LLC
+ * This Software is licenced under the GNU GENERAL PUBLIC LICENSE v3
+ * https://www.gnu.org/licenses/gpl-3.0.txt
+ * https://greatapps4you.us
+ *
+ * Team:
+ * José Esteves de Souza Neto (Lead Engineer)
+ * Renato Magrini (Front-End Developer)
+ * Nathan Parra Ramos (Designer)
+ *
+ * CSSML NDSMD VRS + SNMV SMQL IVB
+ */
+
 import logo from './logo.svg';
 import './App.css';
 
