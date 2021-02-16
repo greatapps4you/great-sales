d5cbefaaebe249f183048acf439baded8c1f707b
Jose E. de Souza N <developer@greatapps4you.us>
2021-02-16 10:40:26 +0100

    [web] corrected wrong naming

--- web/src/components/SideNav.js
+++ web/src/components/SideNav.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import greatapps from '../img/greatapps.png'
+import logo from '../img/greatapps4you_logo.png'
 
 function SideNav() {
-return (
-<aside className="side-nav">
-    <img className="logo" src={greatapps} alt="logo" />
-    <div className="menu">
-      <a href="#">Pedidos</a>
-      <a href="#">Produtos</a>
-      <a href="#">Clientes</a>
-      <a href="#">Fornecedores</a>
-      <a href="#">Transportadoras</a>
-      <a href="#">Vendedores</a>
-    </div>
-</aside>
-)
+    return (
+        <aside className="side-nav">
+            <img className="logo" src={logo} alt="GreatApps4you"/>
+            <div className="menu">
+                <a href="#">Pedidos</a>
+                <a href="#">Produtos</a>
+                <a href="#">Clientes</a>
+                <a href="#">Fornecedores</a>
+                <a href="#">Transportadoras</a>
+                <a href="#">Vendedores</a>
+            </div>
+        </aside>
+    )
 }
 
 export default SideNav;
