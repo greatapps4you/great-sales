5225031e4a4e42f9779694ea8ef6e5a30754465f
Renato Magrini <renato@greatapps4you.us>
2021-02-20 01:43:02 +0100

    [web] productslist formated

--- web/src/components/ProductsList.js
+++ web/src/components/ProductsList.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 class ProductsList extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.state = {
@@ -14,24 +14,24 @@ class ProductsList extends React.Component {
 
     componentDidMount() {
         axios.get('http://localhost:8080/products/list')
-        .then(response => {
-            console.log(response)
-            this.setState({products: response.data})
-            } )
-        .catch(error => {
-            console.log(error)
-            } )
+            .then(response => {
+                console.log(response)
+                this.setState({products: response.data})
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     render() {
 
-        const { products } = this.state
+        const {products} = this.state
 
         return (
-        <div>
-            <h1>Lista de Produtos</h1>
-            {products.map(product => <div>{product.uuid}: {product.sku}: {product.description}</div>)}
-        </div>
+            <div>
+                <h1>Lista de Produtos</h1>
+                {products.map(product => <div>{product.uuid}: {product.sku}: {product.description}</div>)}
+            </div>
 
         )
     }
